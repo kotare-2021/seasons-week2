6ea7f1070aa06790c3b5ca96d0e907e136f4230f
Anastasia Gladun <anastasiagladoun@gmail.com>
2021-06-03 18:48:18 +1200

    initial setup updated

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+// var fileUpload = require('express-fileupload')
 
 const server = express()
+const fs = require('fs')
+
+// const routes = require('./routes')
 
 // Server configuration
 server.use(express.static('public'))
-server.use(express.urlencoded({ extended: false }))
+// server.use(express.urlencoded({ extended: false }))
+// server.use(fileUpload())
 
 // Handlebars configuration
 server.engine('hbs', hbs({ extname: 'hbs' }))
 server.set('view engine', 'hbs')
 
+
 // Your routes/router(s) should go here
 
+server.get('/', (req, res) => {
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+      if (err) return res.status(500).send(err.message)
+      const parseInfo = JSON.parse(data)
+  
+      res.render('home', parseInfo)
+    })
+  })
 
 
 
